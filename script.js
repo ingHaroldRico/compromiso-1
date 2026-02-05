e8c55704ba8617f26ef19107d323bda5555b8491
Harold <ingenieroharold2013@gmail.com>
2026-02-04 19:24:51 -0500

    Primer Cambio

---
+++ script.js
@@ -0,0 +1,6 @@
+function saludar(){
+    const nombre=document.getElementById('nombre').value
+    const apellido=document.getElementById('apellido').value
+
+    alert(`Bienvenido, ${nombre}  ${apellido}.`)
+}
