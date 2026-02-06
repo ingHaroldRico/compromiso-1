eab72e72bc5c25715ec0dde5e3d2b20bae5b306a
Harold <ingenieroharold2013@gmail.com>
2026-02-05 19:45:05 -0500

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function saludar(){
+function ingresar(){
     const nombre=document.getElementById('nombre').value
-    const apellido=document.getElementById('apellido').value
-
-    alert(`Bienvenido, ${nombre}  ${apellido}.`)
+    const rol=document.getElementById('rol').value
+    if(nombre==""){
+        alert("Nombre de Usuario Vacio")
+        return
+        
+    }
+    else if(rol==""){
+        alert("Nombre de Rol Vacio")
+        return
+        
+    }
+    else if(rol=="administrador"){
+        alert("Eres Admistrador, " + nombre)
+        
+    }
+    else if (rol=="editor"){
+        alert("Eres Editor, " + nombre)
+        
+    }
+    else{
+        alert("El Rol" +rol+ " no existe" )
+    }
 }
