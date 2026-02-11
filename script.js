e205ea368b68096c4a29eb1690ebbe12213959ad
Harold <ingenieroharold2013@gmail.com>
2026-02-11 18:53:57 -0500

    primer cambio

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-function ingresar(){
-    const nombre=document.getElementById('nombre').value
-    const rol=document.getElementById('rol').value
-    if(nombre==""){
-        alert("Nombre de Usuario Vacio")
-        return
+let estado=false
+const ElementoBoton=document.getElementById("boton")
+   
+function cambiarTexto() {
+    if(estado==false){
+         
+         ElementoBoton.innerText="Adios"  
+    }
+    else if(estado==true){
         
+        ElementoBoton.innerText="Hola"
+       
     }
-    else if(rol==""){
-        alert("Nombre de Rol Vacio")
-        return
-        
-    }
-    else if(rol=="administrador"){
-        alert("Eres Admistrador, " + nombre)
-        
-    }
-    else if (rol=="editor"){
-        alert("Eres Editor, " + nombre)
-        
-    }
-    else{
-        alert("El Rol" +rol+ " no existe" )
-    }
-}
+    estado = !estado
+}      
